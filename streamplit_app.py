62017c475c059fadf16eba94d836b2428280f49e
Rizqi Prima <rizqiphd@gmail.com>
2025-03-29 16:20:11 +0700

    add lda

---
+++ streamplit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Data Science Portfolio", layout="wide")
+
+st.title("Welcome to My Data Science Portfolio")
+
+st.write("Use the sidebar to navigate between different projects.")
